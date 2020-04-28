50950557b71591f282e61c7ab76d542e2fac6964
Em Poertner <e.poertner@yahoo.com>
2020-04-28 14:44:10 -0400

    starts nav bar, react router integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import SearchForm from './components/SearchForm';
 import SubjectSearchForm from './components/SubjectSearchForm';
-import topper from './book-topper.jpg';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={topper} className="App-topper" alt="topper" />
-        <h1>Book App</h1>
-      </header>
-      <div>
-        <SearchForm />
-        <SubjectSearchForm />
-      </div>
-      <footer className="App-footer">
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div>
+          <Route exact path="/" component={SearchForm}/>
+          <Route path="/subject" component={SubjectSearchForm}/>
+        </div>
+        <footer className="App-footer">
 
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
