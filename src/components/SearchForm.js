893af87d178acfd1af79a55653455cd802a1d213
Em Poertner <e.poertner@yahoo.com>
2020-04-28 11:02:20 -0400

    introduces filter for API results  (bug)

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 
+//imports Google Books API key from untracked .env file
 const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export default class SearchForm extends Component {
@@ -32,34 +33,40 @@ export default class SearchForm extends Component {
     this.fetchBooks();
   }
   
+  //handles fetching list of books from Google Books API
   fetchBooks = async ( 
     searchText = this.state.searchText,
     URL = `https://www.googleapis.com/books/v1/volumes?q=${searchText}&key=${apiKey}&maxResults=20`
 ) => {
     const response = await fetch(URL);
     const books = await response.json();
-    //const filteredBooks = books.filter(book => book.item.volumeInfo.imageLinks.thumbnail)
-    this.setState({ books: books });
-    console.log(books);
+    //this line issue -> says books.filter is not a function
+    const filteredBooks = books.filter(book => book.item.volumeInfo.imageLinks.thumbnail)
+    this.setState({ books: filteredBooks });
+    console.log(filteredBooks);
     };
 
+  //handles fetching more info about a book from Google Books API
   moreDetails = async isbn => {
     const deets = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}&key=${apiKey}&maxResults=1`)
     const details = await deets.json();
     this.setState({ bookDetails: details })
+    //using isbn setState/console.log to help debugging
     this.setState({ currentISBN : isbn})
     console.log(details);
     console.log(isbn);
+    //fetches page from Open Library API
     const libRes = await fetch(`http://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=details&format=json`);
     const openLibRes = await libRes.json();
     this.setState({ openLib: openLibRes });
+    //only shows info if Open Library has a page for that item
     if (Object.keys(this.state.openLib).length > 0)
       this.setState({ didOpenLibRes: true })
     console.log(openLibRes);
     this.setState({ isInfoShowing: true });
   };
 
-
+  //resets state on close; didOpenLibRes needs to be reset because not all books will return Open Lib pages
   handleClose = () => {
     this.setState({ isInfoShowing: false });
     this.setState({ didOpenLibRes: false });
@@ -103,6 +110,7 @@ export default class SearchForm extends Component {
                     {/* <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}/> */}
                     <h3>{book.volumeInfo.title}</h3>
                     {book.volumeInfo.authors.map(author => (<p>{author}</p>))}
+                    {/* line below with etag used for debugging */}
                     {/* <p>{book.etag}</p> */}
                   </li>
                 ))}
@@ -117,9 +125,8 @@ export default class SearchForm extends Component {
 }
 
 //img thumbnail not working -- if book doesn't have image, thows error for whole page -- can't get && working
-//would like to show "View in Open Library" only if they have page for it
-//open library returns json named with ISBN -- how to access state? this.state.openLib[0].info_url
 //some google results don't have an isbn?!?!
+//open library returns json named with ISBN -- how to access state? this.state.openLib[0].info_url
 //break up title and subject onto different "pages"
 //catch if search yields no results
 //need media quieries to display several on a line
