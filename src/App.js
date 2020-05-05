1b42aa9c5edcdadebc17cc4d64af2ce04219eeba
Em Poertner <e.poertner@yahoo.com>
2020-05-04 20:41:53 -0400

    adds comments to code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+//imports
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+//components
 import SearchForm from './components/SearchForm';
 import SubjectSearchG from './components/SubjectSearchG';
 import AuthorSearch from './components/AuthorSearch';
 import Header from './components/Header';
+//style sheet
 import './App.css';
 
+//top level of app
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
         <div>
+          {/* Three navigable routs to search by different fields */}
           <Route exact path="/title" component={SearchForm}/>
           <Route path="/subject" component={SubjectSearchG}/>
           <Route path="/author" component={AuthorSearch}/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
+//imports
 import React from 'react';
 import topper from '../book-topper.jpg';
 import {Link} from 'react-router-dom';
 
+//header
 const Header = () => (
   <header>
     <img src={topper} className="App-topper" alt="topper" />
     <h1>Book App</h1>
     <ul className="main-nav">
+      {/* navigable routs */}
       <li><Link to="/title">Browse by Title</Link></li>
       <li><Link to="/author">Browse by Author</Link></li>
       <li><Link to="/subject">Browse by Subject</Link></li>
