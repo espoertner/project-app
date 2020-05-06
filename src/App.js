39786be2795adc060af6999abf41738caee0aa48
Em Poertner <e.poertner@yahoo.com>
2020-05-05 22:05:05 -0400

    adds 404 rout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //imports
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 //components
 import SearchForm from './components/SearchForm';
 import SubjectSearchG from './components/SubjectSearchG';
 import AuthorSearch from './components/AuthorSearch';
 import Header from './components/Header';
+import NotFound from './components/NotFound';
+
 //style sheet
 import './App.css';
 
@@ -16,10 +18,14 @@ function App() {
       <div className="App">
         <Header />
         <div>
-          {/* Three navigable routs to search by different fields */}
-          <Route exact path="/title" component={SearchForm}/>
-          <Route path="/subject" component={SubjectSearchG}/>
-          <Route path="/author" component={AuthorSearch}/>
+          <Switch>
+            {/* Three navigable routs to search by different fields */}
+            <Route exact path="/" />
+            <Route path="/title" component={SearchForm} />
+            <Route path="/subject" component={SubjectSearchG} />
+            <Route path="/author" component={AuthorSearch} />
+            <Route component={NotFound} />
+          </Switch>  
         </div>
         <footer className="App-footer">
 
